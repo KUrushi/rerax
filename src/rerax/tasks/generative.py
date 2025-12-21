59f4169e4c664f94918e2f40d14dc69a0dabda27
KUrushi <delver.rasping_08@icloud.com>
2025-12-21 14:09:15 +0900

    fix it

--- src/rerax/tasks/generative.py
+++ src/rerax/tasks/generative.py
@@ -21,7 +21,13 @@ class GenerativeTask(Task):
             return (loss * mask).sum() / jnp.maximum(mask.sum(), 1)
         return loss.mean()
 
-    def compute_metrics(self, outputs: chex.Array, batch: dict[str, chex.Array]):
+    def compute_metrics(self, outputs: chex.Array, batch: dict[str, chex.Array], *, mask: chex.Array | None = None):
         predictions = jnp.argmax(outputs, axis=2)
+        correct = (batch['labels'] == predictions).astype(jnp.float32)
 
-        return {"accuracy": (batch["labels"] == predictions).astype(jnp.float32).mean()}
+        if mask is not None:
+            accuracy = (correct * mask).sum() / jnp.maximum(mask.sum(), 1)
+        else:
+            accuracy = correct.mean()
+
+        return {"accuracy": accuracy}
